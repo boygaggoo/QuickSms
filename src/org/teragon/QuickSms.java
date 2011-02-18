fd5208302319f9cad8652a01d06b3c9f23d487fd
Nik Reiman <nik.reiman@gmail.com>
2011-02-18 09:49:34 +0100

    Initial commit

---
+++ src/org/teragon/QuickSms.java
@@ -0,0 +1,15 @@
+package org.teragon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuickSms extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
