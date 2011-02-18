5074a86b96935296590e1810a4f746664d0413bf
Nik Reiman <nik.reiman@gmail.com>
2011-02-18 15:48:22 +0100

    Adding stub classes for contacts

---
+++ src/org/teragon/filters/SmsLengthInputFilter.java
@@ -0,0 +1,11 @@
+package org.teragon.filters;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nik
+ * Date: 2/18/11
+ * Time: 3:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SmsLengthInputFilter {
+}
---
+++ src/org/teragon/model/Contact.java
@@ -0,0 +1,11 @@
+package org.teragon.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nik
+ * Date: 2/18/11
+ * Time: 3:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Contact {
+}
